e79e040ab1b288c812c9c4eed8e451cbbd34092f
MateuszTarka00 <80099249+MateuszTarka00@users.noreply.github.com>
2025-10-17 20:57:43 +0200

    added digital output files and structure

---
+++ Core/Src/DigitalInputs.c
@@ -0,0 +1,30 @@
+/*
+ * DigitalInputs.c
+ *
+ *  Created on: 17 paź 2025
+ *      Author: mateo
+ */
+
+
+#include "DigitalInputs.h"
+
+
+DigitalInput digitalInput[16] =
+{
+		{IN1_GPIO_Port, IN1_Pin},
+		{IN2_GPIO_Port, IN2_Pin},
+		{IN3_GPIO_Port, IN3_Pin},
+		{IN4_GPIO_Port, IN4_Pin},
+		{IN5_GPIO_Port, IN5_Pin},
+		{IN6_GPIO_Port, IN6_Pin},
+		{IN7_GPIO_Port, IN7_Pin},
+		{IN8_GPIO_Port, IN8_Pin},
+		{IN9_GPIO_Port, IN9_Pin},
+		{IN10_GPIO_Port, IN10_Pin},
+		{IN11_GPIO_Port, IN11_Pin},
+		{IN12_GPIO_Port, IN12_Pin},
+		{IN13_GPIO_Port, IN13_Pin},
+		{IN14_GPIO_Port, IN14_Pin},
+		{IN15_GPIO_Port, IN15_Pin},
+		{IN16_GPIO_Port, IN16_Pin},
+};
